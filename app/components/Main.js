b55cd175173fb2ae7b92feecd097432f2d5da611
Jenny Chu <jennychu.sd@gmail.com>
2017-06-03 09:47:23 -0700

    minor edits

---
+++ app/components/Main.js
@@ -0,0 +1,17 @@
+// Include React
+var React = require("react");
+
+// Here we include all of the sub-components
+var Form = require("./children/Form");
+var Saved = require("./children/Saved");
+var Search = require("./children/Search");
+
+// Create the main components
+var Main = React.createClass({
+
+    render: function() {
+        return ()
+    }
+});
+
+module.exports = Main;
--- models/Article.js
+++ models/Article.js
@@ -14,4 +14,4 @@ var ArticleSchema = new Schema({
 });
 
 var Article = mongoose.model("Article", ArticleSchema);
-module.exports = Article;
+module.exports = Article;Article
